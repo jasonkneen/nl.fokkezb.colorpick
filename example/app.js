8c403a76db196b3de485b902a3d76562123119d9
Fokke Zandbergen <mail@fokkezb.nl>
2014-07-03 11:54:20 +0200

    updated example

--- example/app.js
+++ example/app.js
@@ -1,16 +1,27 @@
+var colorpicker = require('nl.fokkezb.colorpicker');
+
 var win = Ti.UI.createWindow({
-  backgroundColor:'white'
+  backgroundColor: '#ddd'
 });
 
-var colorpicker = require('nl.fokkezb.colorpicker');
-Ti.API.info("module is => " + colorpicker);
+var btn = Ti.UI.createButton({
+  top: 25,
+  title: 'Get color value'
+});
 
 var view = colorpicker.createView({
-	top: 0,
-	left: 0,
-	height: 50,
-	width: 50
+  top: 60,
+
+  // initial color (default: black)
+  color: '#f00'
+});
+
+btn.addEventListener('click', function(e) {
+
+  alert('The color: ' + view.color);
+
 });
 
 win.add(view);
+win.add(btn);
 win.open();
